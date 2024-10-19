50862e155adca27f98cadd095d05ef84efa06475
kishan20205 <kishansavaliya258@gmail.com>
2024-10-19 17:44:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let imgbox = document.getElementById("imgbox");
+let qrimage = document.getElementById("qrImage");
+let qrtext = document.getElementById("qrText");
+
+
+function generateQR() {
+    if(qrtext.value.length > 0) {
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+
+        imgbox.classList.add("show-img");
+    }
+    else {
+        qrtext.classList.add("error");
+        setTimeout(() => {
+            qrtext.classList.remove("error");
+        }, 1000);
+    }
+}
